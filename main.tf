d1601d850da4043890729603a1e7b6fc87d17051
Abdl Wahab <wahab@craftyzen.com>
2024-10-03 09:20:37 +0600

    add security group

--- main.tf
+++ main.tf
@@ -3,79 +3,110 @@ provider "aws" {
   profile="muquit"
 }
 
-
-# Create a VPC
-resource "aws_vpc" "main_vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "my_vpc"
+    Name = "my_test_vpc"
   }
 }
 
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "main_igw"
-  }
-}
-
-
-# Create two public subnets
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"  # Subnet with 256 IPs
-  map_public_ip_on_launch = true      # Assign public IPs to instances
-
-  availability_zone = "ap-south-1a"   # Specify the availability zone
-
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  
   tags = {
-    Name = "public_subnet_1"
+    Name = "PublicSubnet1"
   }
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"  # Another subnet with 256 IPs
-  map_public_ip_on_launch = true
-
-  availability_zone = "ap-south-1a"   # Different AZ for redundancy
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "public_subnet_2"
+    Name = "PublicSubnet2"
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
 
+  tags = {
+    Name = "my_test_igw"
+  }
+}
 
-# Create a Route Table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = aws_vpc.my_vpc.id
 
-  # Route all traffic to the Internet Gateway
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "public_route_table"
+    Name = "PublicRouteTable"
   }
 }
 
-# Associate the Route Table with Subnets
-resource "aws_route_table_association" "public_association_1" {
+resource "aws_route_table_association" "rta_public_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "public_association_2" {
+resource "aws_route_table_association" "rta_public_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+
+
+
+
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALBSecurityGroup"
+  }
+}
+
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+
+  tags = {
+    Name = "my_test_alb"
+  }
+}
+
+
+
+
+
+
 
